13a9dc37f1023631cb180b7fe5a15eafe700fd74
Kavya Nama <166195973+kavya-nama@users.noreply.github.com>
2026-05-02 13:40:19 +0530

    feat: implement post operation on /todos endpoint

--- src/index.ts
+++ src/index.ts
@@ -29,22 +29,66 @@ const server = http.createServer((req, res) => {
   }
 
   const url = new URL(`http://${HOST}${req.url}`);
+  console.log(url);
+
   const pathList = url.pathname.split('/');
   pathList.shift();
+  const { method } = req;
+  let rawBody: Buffer[] = [];
+  let body = '';
 
-  switch (pathList[0]) {
-    case '':
-      res.writeHead(200, { 'content-type': 'application/json' });
-      res.end(JSON.stringify({ hello: 'world' }));
-      break;
-    case 'todos':
-      res.writeHead(200, { 'content-type': 'application/json' });
-      res.end(JSON.stringify(todos, null, 2) + '\n');
-      break;
-    default:
-      res.writeHead(404, { 'content-type': 'text/plain' });
-      res.end('Not Found\n');
-  }
+  req.on('data', (chunk) => {
+    rawBody.push(chunk);
+  });
+
+  req.on('end', () => {
+    body = Buffer.concat(rawBody).toString();
+
+    switch (pathList[0]) {
+      case '':
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ hello: 'world' }));
+        break;
+
+      case 'todos':
+        if (method === 'GET') {
+          res.writeHead(200, { 'content-type': 'application/json' });
+          res.end(JSON.stringify(todos, null, 2) + '\n');
+        } else if (method === 'POST') {
+          if (body == '') {
+            res.writeHead(400, { 'content-type': 'text/plain' });
+            res.end('Bad Request\n');
+            return;
+          }
+          try {
+            const json = JSON.parse(body);
+            if (!json['title']) {
+              throw Error("key 'title' does not exist in body");
+            }
+            let id = uuidv4();
+            todos.push({
+              id,
+              title: String(json['title']),
+              createdAt: new Date(),
+              completedAt: null,
+            });
+            res.writeHead(201, { 'content-type': 'application/json' });
+            res.end(JSON.stringify({ id }, null, 2) + '\n');
+          } catch (error: any) {
+            res.writeHead(422, { 'content-type': 'text/plain' });
+            res.end(`Unprocessable Entity ${error.message}\n`);
+          }
+        } else {
+          res.writeHead(404, { 'content-type': 'text/plain' });
+          res.end('Not Found\n');
+        }
+        break;
+
+      default:
+        res.writeHead(404, { 'content-type': 'text/plain' });
+        res.end('Not Found\n');
+    }
+  });
 });
 
 /*
