1914ae3db2d7cc56de8dba761a0075b1c98bfa76
Kavya Nama <166195973+kavya-nama@users.noreply.github.com>
2026-05-02 12:50:56 +0530

    feat: create a simple router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import http from 'node:http';
 
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost:' + PORT;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' });
-  res.end('Hello World!');
+  if (!req.url) {
+    res.writeHead(500, { 'content-type': 'text/plain' });
+    res.end('Internal Server Error\n');
+    console.error(`req.url does not exist`, req);
+    return;
+  }
+
+  const url = new URL(`http://${HOST}${req.url}`);
+  const pathList = url.pathname.split('/');
+  pathList.shift();
+
+  switch (pathList[0]) {
+    case '':
+      res.writeHead(200, { 'content-type': 'application/json' });
+      res.end(JSON.stringify({ hello: 'world' }));
+      break;
+    default:
+      res.writeHead(404, { 'content-type': 'text/plain' });
+      res.end('Not Found\n');
+  }
 });
 
-const port = process.env.PORT || 3000;
+/*
+GET /todos — return all todos
+POST /todos — create a todo
+GET /todos/:id — get one
+PATCH /todos/:id — update it
+DELETE /todos/:id — delete it
+*/
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
