9e21fd981715d2457f2323313ab8e799c3946221
Kavya Nama <166195973+kavya-nama@users.noreply.github.com>
2026-05-02 12:22:00 +0530

    feat: create simple http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello via Bun!');
+import http from 'node:http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
